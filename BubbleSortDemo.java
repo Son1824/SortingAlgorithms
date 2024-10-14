7b6dc6b7f25df2ccd6c2dd2c3be47526e3adb6e1
Son1824 <163498771+Son1824@users.noreply.github.com>
2024-10-15 00:35:53 +0700

    Add files via upload

--- BubbleSortDemo.java
+++ BubbleSortDemo.java
@@ -3,11 +3,11 @@ package SortingAlgorithms;
 import java.util.Arrays;
 
 public class BubbleSortDemo {
-    public static void bubbleSort(int [] arr){
+    public static void bubbleSort(int [] arr) {
         int length = arr.length;
-        for (int i = 0; i < length - 1; i++){
-            for (int j = 0; j < length - i - 1; j++){
-                if (arr[j] > arr [j + 1]){
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -17,8 +17,14 @@ public class BubbleSortDemo {
     }
 
     public static void main(String[] args) {
-        int [] arr = {1, 5, 4, 9, 8, 6};
+        int[] arr = {1, 5, 4, 9, 8, 6};
+
+        System.out.print("Original array: ");
+        for (int num : arr) {
+            System.out.print(num + "\t");
+        }
+
         bubbleSort(arr);
-        System.out.println("Bubble sort to sort incremental arrays: " + Arrays.toString(arr));
+        System.out.println("\nBubble sort to sort incremental arrays: " + Arrays.toString(arr));
     }
 }
